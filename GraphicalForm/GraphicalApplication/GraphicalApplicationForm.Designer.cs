603c721794a74ba5825924cbb7310078c85be625
NotHuzi <43522912+NotHuzi@users.noreply.github.com>
2019-11-03 20:55:02 +0000

    Clean up of functions + rename of variable
    Due to debug issues, I was forced to re name variable names and change the path. Have set up the application to accomondate command and display the output without issues

---
+++ GraphicalForm/GraphicalApplication/GraphicalApplicationForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace GraphicalProgrammingLanguage
+{
+	partial class GraphicalProgrammingLanguageForm
+	{
+		
+		/// Required designer variable
+		
+		private System.ComponentModel.IContainer components = null;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		#region Windows Form Designer generated code
+         
+		
+		private void InitializeComponent()
+		{
+            this.TopMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CodeInputTabControl = new System.Windows.Forms.TabControl();
+            this.SingleLineInputTab = new System.Windows.Forms.TabPage();
+            this.SingleLineOutput = new System.Windows.Forms.RichTextBox();
+            this.SingleLineInputTextbox = new System.Windows.Forms.TextBox();
+            this.LoadFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.GraphicsPictureBox = new System.Windows.Forms.PictureBox();
+            this.DrawingPanel = new System.Windows.Forms.Panel();
+            this.TopMenuStrip.SuspendLayout();
+            this.CodeInputTabControl.SuspendLayout();
+            this.SingleLineInputTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GraphicsPictureBox)).BeginInit();
+            this.DrawingPanel.SuspendLayout();
+            this.SuspendLayout();
+        
+            // Top menu
+            
+            this.TopMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.TopMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.homeToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.TopMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.TopMenuStrip.Name = "TopMenuStrip";
+            this.TopMenuStrip.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.TopMenuStrip.Size = new System.Drawing.Size(1105, 24);
+            this.TopMenuStrip.TabIndex = 0;
+            this.TopMenuStrip.Text = "menuStrip1";
+   
+            // homeToolStripMenuItem
+     
+            this.homeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveImageToolStripMenuItem,
+            this.loadImageToolStripMenuItem});
+            this.homeToolStripMenuItem.Name = "homeToolStripMenuItem";
+            this.homeToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.homeToolStripMenuItem.Text = "File";
+            this.homeToolStripMenuItem.Click += new System.EventHandler(this.HomeToolStripMenuItem_Click);
+          
+            // saveImageToolStripMenuItem
+      
+            this.saveImageToolStripMenuItem.Name = "saveImageToolStripMenuItem";
+            this.saveImageToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.saveImageToolStripMenuItem.Text = "Save Image";
+            this.saveImageToolStripMenuItem.Click += new System.EventHandler(this.saveImageToolStripMenuItem_Click);
+         
+            // loadImageToolStripMenuItem
+       
+            this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
+            this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.loadImageToolStripMenuItem.Text = "Load Image";
+          
+            // helpToolStripMenuItem
+          
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+         
+            // CodeInputTabControl
+          
+            this.CodeInputTabControl.Controls.Add(this.SingleLineInputTab);
+            this.CodeInputTabControl.Dock = System.Windows.Forms.DockStyle.Right;
+            this.CodeInputTabControl.Location = new System.Drawing.Point(533, 0);
+            this.CodeInputTabControl.Margin = new System.Windows.Forms.Padding(2);
+            this.CodeInputTabControl.Name = "CodeInputTabControl";
+            this.CodeInputTabControl.SelectedIndex = 0;
+            this.CodeInputTabControl.Size = new System.Drawing.Size(509, 488);
+            this.CodeInputTabControl.TabIndex = 2;
+         
+            // SingleLineInputTab
+        
+            this.SingleLineInputTab.Controls.Add(this.SingleLineOutput);
+            this.SingleLineInputTab.Controls.Add(this.SingleLineInputTextbox);
+            this.SingleLineInputTab.Location = new System.Drawing.Point(4, 22);
+            this.SingleLineInputTab.Margin = new System.Windows.Forms.Padding(2);
+            this.SingleLineInputTab.Name = "SingleLineInputTab";
+            this.SingleLineInputTab.Padding = new System.Windows.Forms.Padding(2);
+            this.SingleLineInputTab.Size = new System.Drawing.Size(501, 462);
+            this.SingleLineInputTab.TabIndex = 0;
+            this.SingleLineInputTab.Text = "Assignment Part 1";
+            this.SingleLineInputTab.UseVisualStyleBackColor = true;
+        
+            // SingleLineOutput
+       
+            this.SingleLineOutput.BackColor = System.Drawing.Color.White;
+            this.SingleLineOutput.Enabled = false;
+            this.SingleLineOutput.Location = new System.Drawing.Point(14, 79);
+            this.SingleLineOutput.Margin = new System.Windows.Forms.Padding(2);
+            this.SingleLineOutput.Name = "SingleLineOutput";
+            this.SingleLineOutput.Size = new System.Drawing.Size(468, 366);
+            this.SingleLineOutput.TabIndex = 1;
+            this.SingleLineOutput.Text = "";
+      
+            // SingleLineInputTextbox
+         
+            this.SingleLineInputTextbox.Location = new System.Drawing.Point(14, 15);
+            this.SingleLineInputTextbox.Margin = new System.Windows.Forms.Padding(2);
+            this.SingleLineInputTextbox.Multiline = true;
+            this.SingleLineInputTextbox.Name = "SingleLineInputTextbox";
+            this.SingleLineInputTextbox.Size = new System.Drawing.Size(468, 55);
+            this.SingleLineInputTextbox.TabIndex = 0;
+            this.SingleLineInputTextbox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.SingleLineInputEnter);
+         
+            // LoadFileDialog
+           
+            this.LoadFileDialog.FileName = "openFileDialog1";
+         
+            // GraphicsPictureBox
+         
+            this.GraphicsPictureBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.GraphicsPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.GraphicsPictureBox.Margin = new System.Windows.Forms.Padding(2);
+            this.GraphicsPictureBox.Name = "GraphicsPictureBox";
+            this.GraphicsPictureBox.Size = new System.Drawing.Size(531, 488);
+            this.GraphicsPictureBox.TabIndex = 3;
+            this.GraphicsPictureBox.TabStop = false;
+            this.GraphicsPictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.GraphicsPictureBoxPaint);
+      
+            // DrawingPanel
+      
+            this.DrawingPanel.Controls.Add(this.GraphicsPictureBox);
+            this.DrawingPanel.Controls.Add(this.CodeInputTabControl);
+            this.DrawingPanel.Location = new System.Drawing.Point(0, 35);
+            this.DrawingPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.DrawingPanel.Name = "DrawingPanel";
+            this.DrawingPanel.Size = new System.Drawing.Size(1042, 488);
+            this.DrawingPanel.TabIndex = 4;
+   
+            // GraphicalApplicationForm
+     
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1105, 540);
+            this.Controls.Add(this.TopMenuStrip);
+            this.Controls.Add(this.DrawingPanel);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "GraphicalAppplicationForm";
+            this.Text = "GraphicalApplicationInterface";
+            this.TopMenuStrip.ResumeLayout(false);
+            this.TopMenuStrip.PerformLayout();
+            this.CodeInputTabControl.ResumeLayout(false);
+            this.SingleLineInputTab.ResumeLayout(false);
+            this.SingleLineInputTab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.GraphicsPictureBox)).EndInit();
+            this.DrawingPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip TopMenuStrip;
+		private System.Windows.Forms.ToolStripMenuItem homeToolStripMenuItem;
+		private System.Windows.Forms.TabControl CodeInputTabControl;
+		private System.Windows.Forms.TabPage SingleLineInputTab;
+		private System.Windows.Forms.RichTextBox SingleLineOutput;
+		private System.Windows.Forms.TextBox SingleLineInputTextbox;
+		private System.Windows.Forms.OpenFileDialog LoadFileDialog;
+		private System.Windows.Forms.SaveFileDialog SaveFileDialog;
+		private System.Windows.Forms.PictureBox GraphicsPictureBox;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.Panel DrawingPanel;
+		private System.Windows.Forms.ToolStripMenuItem saveImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadImageToolStripMenuItem;
+    }
+}
---
+++ GraphicalForm/GraphicalApplication/GraphicalApplicationForm.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicalProgrammingLanguage
+{
+	public partial class GraphicalProgrammingLanguageForm : Form
+	{
+		ArrayList shapes = new ArrayList();
+		Command command = new Command();
+
+		string previousCommand;
+
+		public GraphicalProgrammingLanguageForm()
+		{
+			InitializeComponent();
+		}
+
+		// Single Line Input
+		private void SingleLineInputEnter(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				// Save the object of the textbox
+				TextBox singleLineInputTextBox = (sender as TextBox);
+
+				if (!String.IsNullOrWhiteSpace(singleLineInputTextBox.Text))
+				{
+					bool validCommand = false;
+					string errorMessage = "";
+
+					string[] splitUserInput = command.SplitUserInput(singleLineInputTextBox.Text.Trim());
+
+					string commandString = splitUserInput[0];
+					string commandParameters = string.Join(" ", splitUserInput.Skip(1).ToArray());
+
+					validCommand = command.ValidateCommand(1, commandString, commandParameters, out errorMessage);
+
+					if (!validCommand)
+					{
+						// If the user has not entered a valid command
+						this.SingleLineOutput.SelectionColor = Color.Red;
+						this.SingleLineOutput.AppendText(errorMessage + "\n");
+						this.SingleLineOutput.ScrollToCaret();
+					}
+					else if(validCommand && String.Equals(errorMessage, "multiline"))
+					{
+						// Do nothing
+					}
+					else
+					{
+						if (commandString.ToLower() == "run")
+						{
+							RunFileCommand(commandParameters);
+						}
+						else
+						{
+							// Else if the user has entered a valid command
+							// Add the command to the textbox
+							this.SingleLineOutput.SelectionColor = Color.Green;
+							this.SingleLineOutput.AppendText(SingleLineInputTextbox.Text.Trim() + "\n");
+							this.SingleLineOutput.ScrollToCaret();
+
+							// Set the TextBox value to be an empty string
+							singleLineInputTextBox.Text = "";
+
+							CallCommand(commandString, commandParameters);
+						}
+					}
+				}
+			}
+			else if(e.KeyCode == Keys.Up)
+			{
+				if (!String.IsNullOrWhiteSpace(previousCommand))
+				{
+					this.SingleLineInputTextbox.Text = previousCommand;
+				}
+			}
+		}
+
+		public void RunFileCommand(string filePath)
+		{
+			// Output to the user that file is attempting to be loaded.
+			this.SingleLineOutput.SelectionColor = Color.Green;
+			this.SingleLineOutput.AppendText("Attempting to load file (" + filePath + ") \n");
+			this.SingleLineOutput.ScrollToCaret();
+
+			SingleLineInputTextbox.Text = "";
+
+			if (filePath.Contains('"'))
+			{
+				filePath = filePath.Replace('"', ' ').Trim();
+			}
+
+			string errorMessage;
+			if (!command.ValidateFile(filePath, ".txt", out errorMessage))
+			{
+				this.SingleLineOutput.SelectionColor = Color.Red;
+				this.SingleLineOutput.AppendText(errorMessage);
+				this.SingleLineOutput.ScrollToCaret();
+			}
+			else
+			{
+				// Attempt to load the file
+				System.IO.StreamReader file = new System.IO.StreamReader(filePath);
+
+				string line;
+				int lineNumber = 0;
+
+				// Global input line variable
+				bool inMultiLine = false;
+				string globalCommand = "";
+				string globalParameters = "";
+
+				while ((line = file.ReadLine()) != null)
+				{
+					// Iterate the file line by line
+					// Increment the line number
+					lineNumber++;
+
+					bool validCommand = false;
+					string[] lineUserInputSplit = line.Split(' ');
+
+					// Split the line of the file into command and parameters
+					string lineCommand = lineUserInputSplit[0];
+					string lineParameters = string.Join(" ", lineUserInputSplit.Skip(1).ToArray());
+
+					// If the command is in a multiline loop
+					if (inMultiLine)
+					{
+						string multiLineParameters = globalParameters + "\n\r" + lineCommand + " " + lineParameters;
+						// If it is validate the command
+						if (command.ValidateCommand(lineNumber, globalCommand, multiLineParameters, out errorMessage))
+						{
+							// Concatenate the command and parameters with \n\r
+							globalParameters = globalParameters + "\n\r" + lineCommand + " " + lineParameters;
+
+							validCommand = true;
+						}
+						else
+						{
+							// Return the error message to the user and break
+							this.SingleLineOutput.SelectionColor = Color.Red;
+							this.SingleLineOutput.AppendText("Line: " + lineNumber + " - " + errorMessage + "\n");
+							this.SingleLineOutput.ScrollToCaret();
+							continue;
+						}
+					}
+					else
+					{
+						if (String.Equals(lineCommand.ToLower(), "if") || String.Equals(lineCommand.ToLower(), "loop"))
+						{
+							// If the command string lowered contain 'if' or 'loop'
+
+							// Split the parameters based on ';'
+							string[] lineParametersSplit = lineParameters.Split(';');
+
+							if (lineParametersSplit.Length == 1)
+							{
+								// If the parameters is of length 1. It is a multiline loop
+
+								// Set the global command string to be 'if'/'loop'
+								globalCommand = lineCommand;
+								globalParameters = lineParameters;
+								// Set the multiLine bool to true
+								inMultiLine = true;
+
+								validCommand = command.ValidateCommand(lineNumber, globalCommand, globalParameters, out errorMessage);
+							}
+							else
+							{
+								// Else it is a singleline loop
+								// Set the global multiline bool to false
+								inMultiLine = false;
+								// Set the global command string to the user input
+								globalCommand = lineCommand;
+								// Set the global parameters string to the user input
+								globalParameters = lineParameters;
+
+								// Pass each line to the validator along with the line number
+								validCommand = command.ValidateCommand(lineNumber, globalCommand, globalParameters, out errorMessage);
+							}
+						}
+						else
+						{
+							// Else
+							// Set the global multiline bool to false
+							inMultiLine = false;
+							// Set the global command string to the user input
+							globalCommand = lineCommand;
+							// Set the global parameters string to the user input
+							globalParameters = lineParameters;
+
+							// Pass each line to the validator along with the line number
+							validCommand = command.ValidateCommand(lineNumber, globalCommand, globalParameters, out errorMessage);
+						}
+					}
+
+					if (!validCommand)
+					{
+						// If the user has not entered a valid command
+						this.SingleLineOutput.SelectionColor = Color.Red;
+						this.SingleLineOutput.AppendText("Line: " + lineNumber + " - " + errorMessage + "\n");
+						this.SingleLineOutput.ScrollToCaret();
+					}
+					else if (validCommand && String.Equals(errorMessage, "multiline"))
+					{
+						// Add the command to the textbox
+						this.SingleLineOutput.SelectionColor = Color.Green;
+						this.SingleLineOutput.AppendText("Line: " + lineNumber + " - " + line + "\n");
+						this.SingleLineOutput.ScrollToCaret();
+					}
+					else
+					{
+						// Else if the user has entered a valid command
+						// Add the command to the textbox
+						this.SingleLineOutput.SelectionColor = Color.Green;
+						this.SingleLineOutput.AppendText("Line: " + lineNumber + " - " + line + "\n");
+						this.SingleLineOutput.ScrollToCaret();
+
+						CallCommand(globalCommand, globalParameters);
+					}
+				}
+			}
+		}
+
+		public void CallCommand(string commandString, string commandParameters)
+		{
+			try
+			{
+				if (command.ExecuteCommand(shapes, commandString, commandParameters))
+				{
+					this.GraphicsPictureBox.Refresh();
+					previousCommand = commandString + " " + commandParameters;
+				}
+			}
+			catch (FormatException ex)
+			{
+				Console.WriteLine(ex);
+
+				// If the user has not entered a valid command
+				this.SingleLineOutput.SelectionColor = Color.Red;
+				this.SingleLineOutput.AppendText("A string value tried to parse to an integer" + "\n");
+				this.SingleLineOutput.ScrollToCaret();
+			}
+			catch (IndexOutOfRangeException ex)
+			{
+				Console.WriteLine(ex);
+
+				// If the user has not entered a valid command
+				this.SingleLineOutput.SelectionColor = Color.Red;
+				this.SingleLineOutput.AppendText("The wrong number of parameters have been passed." + "\n");
+				this.SingleLineOutput.ScrollToCaret();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+				// If the user has not entered a valid command
+				this.SingleLineOutput.SelectionColor = Color.Red;
+				this.SingleLineOutput.AppendText("An error occured" + "\n");
+				this.SingleLineOutput.ScrollToCaret();
+			}
+		}
+
+        private void GraphicsPictureBoxPaint(object sender, PaintEventArgs e)
+		{
+			Graphics g = e.Graphics;
+
+			for(int i = 0; i < shapes.Count; i++)
+			{
+				Shape s = (Shape) shapes[i];
+				s.Draw(g);
+			}
+		}
+
+		private void HomeToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.DrawingPanel.Visible = true;
+		}
+
+		private void saveImageToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (SaveFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				Bitmap bmp = new Bitmap(this.GraphicsPictureBox.Width, this.GraphicsPictureBox.Height);
+				this.GraphicsPictureBox.DrawToBitmap(bmp, this.GraphicsPictureBox.ClientRectangle);
+
+				bmp.Save(SaveFileDialog.FileName);
+
+				this.SingleLineOutput.SelectionColor = Color.Green;
+				this.SingleLineOutput.AppendText("Image saved to " + SaveFileDialog.FileName);
+				this.SingleLineOutput.ScrollToCaret();
+			}
+		}
+	}
+}
