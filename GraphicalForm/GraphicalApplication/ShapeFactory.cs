2760143ba8160f5eed3f4fccb7d3c2b31380d306
NotHuzi <43522912+NotHuzi@users.noreply.github.com>
2019-11-03 21:43:01 +0000

    Created shape files
    Created the foundation for circle, triangle and rectangle shapes.

---
+++ GraphicalForm/GraphicalApplication/ShapeFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicalApplication
+{
+	public class ShapeFactory
+	{
+		/// <summary>
+		/// Shape factory to return the requested shape.
+		/// </summary>
+		/// <param name="shapeType">String: The shape to be returned to the user.</param>
+		/// <returns>Shape: A refernece to the requested shape.</returns>
+		public Shape GetShape(string shapeType)
+		{
+			shapeType = shapeType.ToUpper().Trim();
+
+			if(shapeType == "RECTANGLE")
+			{
+				return new Rectangle();
+			}
+			else if(shapeType == "CIRCLE")
+			{
+				return new Circle();
+			}
+			else if(shapeType == "TRIANGLE")
+			{
+				return new Triangle();
+			}
+			else if(shapeType == "MOVETO")
+			{
+				return new Line();
+			}
+			else
+			{
+				return new Rectangle();
+			}
+		}
+	}
+}
